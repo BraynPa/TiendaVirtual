e7e1df4ae3a8dfb348a523f662eccaadde4143aa
BraynPa <bry1301@gmail.com>
2024-12-15 13:45:47 -0500

    Categorias

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Category;
 class CategorySeeder extends Seeder
 {
     /**
@@ -12,6 +12,19 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        /*collect([
+            'Electronics',
+            'Books',
+            'Clothing',
+            'Home & Kitchen',
+        ])->each(fn ($category) => Category::create(['name' => $category]));*/
+        collect([
+            'Electronics',
+            'Books',
+            'Clothing',
+            'Home & Kitchen',
+        ])->each(function($category){
+            Category::firstOrCreate(['name' => $category]);
+        });
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@gmail.com',
+            'password' => 'testroot'
         ]);
+
+        $this->call(CategorySeeder::class);
     }
 }
