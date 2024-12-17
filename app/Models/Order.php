ac3e823805c111c73ceba5d6536f25afcfa00192
BraynPa <bry1301@gmail.com>
2024-12-17 15:07:49 -0500

    MercadoPago

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,7 +9,16 @@ class Order extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected $casts = [
+        'api_response' => 'array'
+    ];
     public function orderDetails(){
         return $this->hasMany(OrderDetail::class);
+    }
+    protected static function booted(): void
+    {
+        static::creating(function (Order $order) {
+            $order->uuid = str()->uuid();
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,24 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShoppingCartController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::resource('products', ProductController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('products', ProductController::class);
+  
+    Route::resource('orders', OrderController::class)->only('store');
+    Route::get('callback/{order:uuid}', [OrderController::class, 'callback'])->name('config');
+  
+    Route::post('add-to-cart/{product}', [ShoppingCartController::class, 'store']);
+    Route::get('checkout', [ShoppingCartController::class, 'index'])->name('checkout');
+  });
 
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::post('/add-to-cart/{product}',[ShoppingCartController::class, 'store']);
-Route::get('checkout',[ShoppingCartController::class, 'index'])->name('checkout');
